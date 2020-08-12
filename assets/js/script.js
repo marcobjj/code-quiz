b6d961eae9b0dea9a4ba254857889df0f4aa34c3
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-08-12 15:08:38 -0700

    high scores and timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var quizList = document.querySelector("#list");
 var quizQuestion = document.querySelector("#question");
+var quizResult = document.querySelector("#result");
+var finalScore = document.querySelector("#final-score");
+var submitScore = document.querySelector("#submit-score");
+var userScore = document.querySelector("#user-scores");
 
 
 var questionObject1 = {};
@@ -18,7 +22,7 @@ questionObject3.choices = ["Curly brackets", "Brackets","Comma", "Parenthesis"];
 questionObject3.answer = 3;
 
 var questionObject4 = {};
-questionObject4.question = "A very useful tool during development and debudding for printing out content to the debugger is:";
+questionObject4.question = "A very useful tool during development and debugging for printing out content to the debugger is:";
 questionObject4.choices = ["Terminal bash", "Javascript","Console.log", "For loops"];
 questionObject4.answer = 2;
 
@@ -32,6 +36,10 @@ var quizArray = [questionObject1, questionObject2, questionObject3,questionObjec
 
 var currentQuestion = 0;
 var correctAnswer = 0;
+var totalScore = 0;
+var totalTime = 0;
+
+var timeCount ;
 
 
 var startQuiz = function (){
@@ -40,12 +48,25 @@ currentQuestion = 0;
 
 reset();
 addQuestion();
+clearTimeAndScore();
+
+}
+
+var clearTimeAndScore = function ()
+{
+
+    totalScore = 0;
+    totalTime = 35000;
+    timeCount = setInterval(intervalHandler,1000);
 
 }
 
 var reset = function(){
 
    if( quizList.hasChildNodes()  ) quizList.textContent = '';
+
+  
+
 
 }
 
@@ -92,12 +113,105 @@ var clickHandler = function (event) {
 
 }
 
+var intervalHandler = function ()
+{
+
+    totalTime <= 0 ? endQuiz() : totalTime -= 1000;
+
+}
+
+var submitScoreHandler = function (event)
+{
+event.preventDefault();
+
+
+
+var input = document.querySelector("#initials");
+var initials = input.value;
+var id = getStorage();
+
+var userScore = {
+
+    name: initials,
+    score: totalScore
+}
+
+localStorage.setItem(id, JSON.stringify(userScore));
+
+}
+
 var validateAnswer = function (id) {
 
-if(id == correctAnswer) alert("Correct!");
-else alert("Wrong!");
+if(id == correctAnswer) correct();
+else wrong();
 
 nextQuestion();
+
+}
+
+var correct = function ()
+{
+  totalScore += 20;
+  giveFeedback("Correct!");
+   
+}
+
+var wrong = function ()
+{
+ totalTime -= 10000;
+ giveFeedback("Wrong!");
+
+
+}
+
+var giveFeedback = function(feedback){
+
+    var h2 = quizResult.querySelector("h2");
+    h2.textContent = feedback;
+
+}
+
+var fetchScores = function()
+{
+   var scoresArray = [];
+
+   var scoresArrayLength = getStorage();
+
+   for (var i = 0; i < scoresArrayLength; i++)
+   {
+
+    scoresArray.push(JSON.parse(window.localStorage.getItem(i)));
+   }
+
+   scoresArray.sort(function(a, b) { 
+    return b.score -a.score;
+  });
+
+  printScores(scoresArray);
+
+}
+
+var printScores = function (scoresArray){
+
+    userScore.innerHTML = '';
+
+    for(var i=0; i < scoresArray.length; i++)
+    {
+        var order = 1+i;
+
+        var li = document.createElement("li");
+
+
+        li.appendChild(document.createTextNode(order+". "+scoresArray[i].name +" - "+scoresArray[i].score ));
+
+        li.setAttribute("class","score");
+
+        userScore.appendChild(li);
+
+
+       // console.log(scoresArray[i].name +" - "+scoresArray[i].score);
+    }
+
 
 }
 
@@ -117,11 +231,27 @@ var nextQuestion = function (){
 
 var endQuiz = function (){
 
-alert("the end!");
+finalScore.textContent = "Your final score is: "+totalScore;
+clearInterval(timeCount);
 
+}
+
+
+function getStorage() {
+
+    var values = [],
+        keys = Object.keys(localStorage),
+        i = keys.length;
+
+  //  while ( i-- ) {
+  //      values.push( localStorage.getItem(keys[i]) );
+  //  }
+
+   return i
 }
 
 startQuiz();
 
 quizList.addEventListener('click', clickHandler)
+submitScore.addEventListener('click', submitScoreHandler)
 
